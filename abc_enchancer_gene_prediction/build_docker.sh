f77fad03d5c8f4d4fffff9db8ae9afb796f03ec6
Dave Roberson <davidroberson@users.noreply.github.com>
2023-04-28 01:44:39 +0000

    sbpack

---
+++ abc_enchancer_gene_prediction/build_docker.sh
@@ -0,0 +1,4 @@
+#git clone https://github.com/broadinstitute/ABC-Enhancer-Gene-Prediction.git
+mv Dockerfile ABC-Enhancer-Gene-Prediction
+cd ABC-Enhancer-Gene-Prediction/
+#docker build -t images.sb.biodatacatalyst.nhlbi.nih.gov/andrewblair/cardiac-compendium:2023042401 ./
