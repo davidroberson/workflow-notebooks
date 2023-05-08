9d7f1d8b3ba4a52dc5ff6c898f3d2823e7909804
davidroberson <david.roberson@gmail.com>
2023-05-08 12:28:16 +0000

    re org

--- abc_enhancer_gene_prediction/Rcwl/make_candidate_regions.cwl.R
+++ abc_enhancer_gene_prediction/Rcwl/make_candidate_regions.cwl.R
@@ -1,29 +1,31 @@
+# libraries ----
 library(Rcwl)
 library(tidyverse)
 
-#input ports
+# input ports ----
 input_ports = list(
   InputParam(id = "narrow_peak", type = "File"),
-  InputParam(id = "bam", type = "File", 
-             secondaryFiles = list(pattern = ".bai"), value = list("BAM")),
+  InputParam(id = "bam", type = "File"),
   InputParam(id = "chr_sizes", type = "File"),
   InputParam(id = "regions_blocklist", type = "File"),
   InputParam(id = "regions_includelist", type = "File"))
 
-#output ports
+# output ports ----
 output_ports = list(
   OutputParam(id = "candidate_regions", type = "File", 
               glob = '*candidateRegions.bed'),
   OutputParam(id = "counts", type = "File", 
               glob = '*Counts.bed'))
 
-#requirements
+#requirements ----
 docker = "images.sb.biodatacatalyst.nhlbi.nih.gov/andrewblair/cardiac-compendium:2023042401"
-shell_script = Dirent(entryname = "make_candidate_regions.sh", read_file("make_candidate_regions.sh"), writable = FALSE)
-
-#hints
+shell_script = Dirent(entryname = "make_candidate_regions.sh", 
+                      read_file("../scripts/make_candidate_regions.sh"), 
+                      writable = FALSE)
+#hints ----
 hints = list(class = "sbg:SaveLogs", value = "*.sh")
 
+#cwl process object ----
 make_candidate_regions <- cwlProcess(
   baseCommand = list("bash", "make_candidate_regions.sh"),
   inputs = do.call(InputParamList, c(input_ports)),
@@ -35,37 +37,6 @@ make_candidate_regions <- cwlProcess(
     requireInitialWorkDir(list(shell_script))),
   hints = list(hints))
 
-make_candidate_regions <- addMeta(
-  cwl = make_candidate_regions,
-  label = " MACS2 Call Candidate Regions",
-  doc = read_file("doc.md"))
 
 #$namespaces:
 #sbg: https://sevenbridges.com
-
-#write cwl to file
-writeCWL(make_candidate_regions, outdir = "./")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- setup-files/setup.R
+++ setup-files/setup.R
@@ -1,6 +1,9 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install("Rcwl")
+#BiocManager::install("Rcwl")
 
 system("pip3 install sbpack")
 
+BiocManager::install("rworkflow/Rcwl")
+
+install.packages("tidycwl")
